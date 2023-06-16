713ccc5c8a733317af3a302eb0b762884e0bc79b
Aleksandra Redichkina <103931094+AMRedichkina@users.noreply.github.com>
2023-06-16 20:15:21 +0200

    Create lib.js

---
+++ lib.js
@@ -0,0 +1,26 @@
+var socialMedia = [
+  "whatsapp",
+  "instagram",
+  "twitter",
+  "snapchat",
+  "tiktok",
+  "youtube",
+  "wechat",
+  "facebook",
+  "telegram",
+  "bbm",
+  "palmchat",
+  "gmail",
+  "2go",
+  "linkedin",
+  "reddit",
+  "hangouts",
+  "qq",
+  "twitch",
+  "douyin"
+];
+
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
